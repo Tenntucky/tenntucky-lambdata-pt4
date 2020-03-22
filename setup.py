b0997b78b99a5a3fe7bdc2f3f3e8959d7e3c8180
Kole Goldsberry <kole.goldsberry@gmail.com>
2020-03-22 16:12:00 -0500

    Prepare to push to PYpi

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py file
+from setuptools import find_packages, setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name="tenntucky-lambdata-pt4",
+    version="1.0",
+    author="Kole Goldsberry",
+    author_email="datacreativellc@gmail.com",
+    description="For example purposes",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/Tenntucky/tenntucky-lambdata-pt4",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
