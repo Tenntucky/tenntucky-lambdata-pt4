b41cc6058e47e50c2ebb133141381fc4ca8bc2bd
Kole Goldsberry <kole.goldsberry@gmail.com>
2020-03-22 19:47:06 -0500

    Add docstring

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,7 +1,16 @@
 
 # my_lambdata/my_mod.py
 
+
+
 def enlarge(n):
+    """
+    Multiply a number by 100
+
+    Param: n (numeric / float / int) the number to enlarge
+
+    Return the enlarged number (numeric)
+    """
     return n * 100
 
 if __name__ == "__main__":
